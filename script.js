306d12fd024fb48c2f52619e40ead5ad1a97344e
script <script@gmail.com>
2023-12-03 11:08:51 +0500

    first

---
+++ script.js
@@ -0,0 +1,76 @@
+// console.log( 'строка');
+// console.log(22);
+// console.log(true);
+// console.log(false);
+
+
+var names = 'бахриддин'
+var year = 40
+var boolen = true
+var und
+
+
+// console.log( names);
+// console.log( year);
+// console.log( boolen);
+// console.log( und);
+
+
+// console.log('6 + 3 = ' + ( 6 + 3));
+
+
+// var str = prompt( ' ведите свое имя')
+
+// console.log(str);
+
+
+// var num = +prompt(' nidcicidnc')
+
+
+// console.log(num);
+
+// var year = +prompt('cecedecdc')
+
+// console.log( 'вам ' + (2023 - year) + ' лет' );
+
+
+
+var name = prompt( 'Введите свое имя')
+console.log(name);
+
+var num = +prompt(' Введите год рождения')
+console.log( 'Вам ' + (2023 - num) + ' лет');
+
+
+
+var solve = +prompt( ' Решите пример 20 + 10 = ?')
+var solvee = +prompt( ' Решите пример 100 - 10 = ?')
+var solverr = +prompt( ' Решите пример 20 * 40 = ?')
+var solvew = +prompt( ' Решите пример 3000 / 10 = ?')
+var solveee = +prompt( ' Решите пример 25 % 10 = ?')
+
+
+alert(' Введите три числа ')
+
+var X, Y, Z, res;
+
+
+X = prompt( 'первое число ')
+Y = prompt( 'первое число ')
+Z = prompt( 'первое число ')
+
+
+
+
+alert( 'откройте панель разработчика')
+
+console.log( ' Пример 1: 20 + 10 = 30 ' + ' Ваш ответ ' + solve );
+console.log( ' Пример 2: 100 - 10 = 90 ' + ' Ваш ответ ' + solvee );
+console.log( ' Пример 3: 20 * 40 = 800 ' + ' Ваш ответ ' + solverr );
+console.log( ' Пример 4: 3000 / 10 = 300 ' + ' Ваш ответ ' + solvew );
+console.log( ' Пример 5: 25 % 10 = 5 ' + ' Ваш ответ ' + solveee );
+
+res = console.log( (X + Y + Z) / 3);
+
+
+
